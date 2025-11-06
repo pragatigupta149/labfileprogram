1cff8dc3b414c8e7944d4c490da324cfee1a9e4c
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-06 11:40:53 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i,f,c,a,b,m;
+    i=sizeof(int);
+    f=sizeof(float);
+    m=sizeof(char);
+    printf("integer=%d \n",i);
+    printf("float=%d  \n",f);
+    printf("character=%d   \n",m);
+   return 0;
+}
